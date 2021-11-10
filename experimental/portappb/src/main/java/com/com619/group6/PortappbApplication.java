b5300e1c2c21948bf0555da13f610cc56d8d1743
Grahame White <4whitg16@solent.ac.uk>
2021-11-10 19:23:39 +0000

    Keep linter happy

--- experimental/portappb/src/main/java/com/com619/group6/PortappbApplication.java
+++ experimental/portappb/src/main/java/com/com619/group6/PortappbApplication.java
@@ -3,7 +3,10 @@ package com.com619.group6;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//CHECKSTYLE:OFF
+// Annotation doesn't allow the class to be declared as final
 @SpringBootApplication
+//CHECKSTYLE:ON
 public class PortappbApplication {
     private PortappbApplication() {
     }
