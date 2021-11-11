d85f25f16c978454f2031f74954f232122c664c6
Grahame White <4whitg16@solent.ac.uk>
2021-11-11 10:46:55 +0000

    Suppress violation around SpringBootApplication

--- experimental/portappb/src/main/java/com/com619/group6/PortappbApplication.java
+++ experimental/portappb/src/main/java/com/com619/group6/PortappbApplication.java
@@ -3,7 +3,9 @@ package com.com619.group6;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//CHECKSTYLE:OFF:
 @SpringBootApplication
+//CHECKSTYLE:ON:
 public class PortappbApplication {
     private PortappbApplication() {
     }
